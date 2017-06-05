6c980fe553dd39a50b989015aa9ad454ef4a7933
Armadeas Wijaya <ketikdeas@yahoo.com>
2017-06-05 13:28:56 +0700

    first commit

---
+++ client/templates/posts/post_list.js
@@ -0,0 +1,19 @@
+var postData = [
+{
+	title: 'Introduction Telescope',
+	author: 'Sacha Greif',
+	url: 'http://facebook.com/'
+},
+{
+	title: 'Meteor',
+	author: 'Kiamat',
+	url: 'http://erp.ptcs.co.id'
+},
+{
+	title: 'Corporate Management System',
+	author: 'PTCS',
+	url: 'http://cms.ptcs.co.id'
+}];
+Template.postList.helpers({
+	posts: postData
+});
